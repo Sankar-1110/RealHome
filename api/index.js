c21f8753552b81b4879801869e7993d52852dc97
NAKKA SAI SANKAR <imt_2022074@iiitm.ac.in>
2024-08-06 17:12:22 +0530

    finalchanges2

--- api/index.js
+++ api/index.js
@@ -17,12 +17,13 @@ mongoose.connect(process.env.URI).then(()=>{
 })
 app.use("/api/auth",authRouter);   
 app.use("/api/user",userRouter);    
-app.use("/api/listing",listingRouter);    
+app.use("/api/listing",listingRouter); 
+
 app.use(express.static(path.join(path.resolve(), 'my-project/dist')));
 
 // Anything that doesn't match the API routes should be handled by the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.resolve(), 'my-project/dist', 'index.html'));
+  res.sendFile(path.join(path.resolve(), 'my-project','dist', 'index.html'));
 });
 
 app.use((err,req,res,next)=>{
